7ea3f9e3e399599f113ddce655f808f2fd74dee2
jlordo112 <johnlay2@illinois.edu>
2019-09-17 18:08:32 -0500

    feat: takes in wav file and returns loudest frequency

--- music_analysis.py
+++ music_analysis.py
@@ -1,4 +1,3 @@
-#import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.fftpack import fft,fftfreq
 import numpy as np
@@ -8,8 +7,6 @@ def loudest_freq(wav_file):
     samples = len(data)
     abs_fft = np.abs(fft(data)[:samples//2])
     freqs = fftfreq(samples, 1/samplerate)[:samples//2] #only need positive first halves
-    #plt.plot(freqs, abs_fft)
-    #plt.show()
     max_v = np.amax(abs_fft)
     max_i = np.where(abs_fft == max_v)[0]
     max_f = float(freqs[max_i])
