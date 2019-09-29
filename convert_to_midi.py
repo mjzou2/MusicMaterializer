ccb83a3f98422bb9c9bda170254e9a32f79caf66
peiyanw2 <peiyanw2@illinois.edu>
2019-09-29 12:57:16 -0500

    py: (bug_fix)tested and fixed some bug for convert_to_midi. Now it can work with frequency_to_notes

--- convert_to_midi.py
+++ convert_to_midi.py
@@ -1,21 +1,28 @@
 import midi
+import frequency_to_notes
 
-
+    
 class ConvertToMidi:
     noteName = []
     bpm = []
     constVelocity = 30
-    timeInt = 1
+    timeInt = 240
 
     # constructor
     def __init__(self, notes, tempo):
         print("constructing...")
         self.noteName = notes
-        self.bpm = tempo
+        for i in range(len(self.noteName)):
+            for j in range (len(self.noteName[i])):
+                self.noteName[i][j] = round(self.noteName[i][j])
 
+        self.bpm = tempo
+                
     # convert notes and output the midi file
     def toMidi(self):
         print("called toMidi")
+        print(self.noteName)
+        print(self.bpm)
         # create midi pattern and track
         pattern = midi.Pattern()
         track = midi.Track()
@@ -26,9 +33,14 @@ class ConvertToMidi:
             for j in range(len(self.noteName[i])):
                 on = midi.NoteOnEvent(tick = 0, velocity = self.constVelocity, pitch = self.noteName[i][j])
                 track.append(on)
-            
+
             for k in range(len(self.noteName[i])):
-                off = midi.NoteOffEvent(tick = self.timeInt, velocity = self.constVelocity, pitch = self.noteName[i][j])
+                if k == 0:
+                    off = midi.NoteOffEvent(tick = self.timeInt, pitch = self.noteName[i][k])
+                else:
+                    off = midi.NoteOffEvent(tick = 0, pitch = self.noteName[i][k])
+
+                track.append(off)
 
         print("creating eot...")
         eot = midi.EndOfTrackEvent(tick = 1)
@@ -40,3 +52,18 @@ class ConvertToMidi:
         print ("Saving ...")
         midi.write_midifile("output.mid", pattern)
 
+        print ("Finished")
+
+"""
+print ("running test")
+
+
+notes = [[31, 57], [69], [48, 38]]
+print (notes)
+
+print(frequency_to_notes.frequencyToNote([[440,220],[329.628,493.883]]))
+test = ConvertToMidi(frequency_to_notes.frequencyToNote([[440,220],[329.628,493.883]]), 90)
+test.toMidi()
+
+print("finished")
+"""
