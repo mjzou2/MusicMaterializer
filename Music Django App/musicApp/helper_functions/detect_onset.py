59c7967af02a6466706eddc0fb380576a1a63263
jlordo112 <johnlay2@illinois.edu>
2019-12-08 12:17:31 -0600

    helper func

---
+++ Music Django App/musicApp/helper_functions/detect_onset.py
@@ -0,0 +1,12 @@
+from scipy.io import wavfile
+
+def detect_onset(wav):
+    rate, data = wavfile.read(wav)
+    if data.ndim > 1: #if two (plus?) channels
+        data = [max(data[i]) for i in range(len(data))]
+    min_amp = 10000 #seems okay?
+    for i, sample in enumerate(data):
+        if abs(sample) > min_amp:
+            return i // (rate / 1000)
+    return -1
+
---
+++ Music Django App/musicApp/helper_functions/midi-to-pdf.sh
@@ -0,0 +1,8 @@
+# accepts a MIDI file and outputs a pdf using lilypond
+echo "converting midi to music.ly..."
+midi2ly $1 --output=music.ly
+echo "converting music.ly to pdf..."
+lilypond music.ly
+echo "Done."
+rm music.ly
+rm music.midi	
